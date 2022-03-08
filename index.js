72b713a5c53f9bf65982ada930dd5ce8757ba99e
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-08 14:27:23 -0500

    Create questions ask for the user

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const generateTeam = require("./src/pageTEmplate");
+
+//Array for answer and question
+const buildTeam = [];
+
+//Create questions asked for the user
+const questions =  async () => {
+    const answers = await inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the id number?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the email?",
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is the role?",
+            choices: ["Engineer", "Intern", "Manager",]
+        },
+
+    ])
