1dbfa5f1579353433e5f53ff1a9be12c5435ced9
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-08 15:23:28 -0500

    creating html for engineer

--- src/pageTEmplate.js
+++ src/pageTEmplate.js
@@ -18,3 +18,22 @@ const generateTeam = (team) => {
     </div>
      `
     };
+
+     // create the html for the engineer
+     const generateEngineer = (engineer) => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-success text-light">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">Id:${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">Gihub: <a href="${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div> 
+    `
+    };
