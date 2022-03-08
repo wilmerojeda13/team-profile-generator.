4fa92ce25904652315e1e77f003763e81d4517f2
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-08 15:24:40 -0500

    creating html for intern

--- src/pageTEmplate.js
+++ src/pageTEmplate.js
@@ -37,3 +37,22 @@ const generateTeam = (team) => {
     </div> 
     `
     };
+
+    //create the html for interns
+    const generateIntern = (intern) => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-secondary text-light">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+     `
+    };
