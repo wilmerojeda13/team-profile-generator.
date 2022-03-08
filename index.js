c98abd7a8422ae46f07bc0acf29d0c75af159019
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-08 14:24:15 -0500

    export the requires modules and libraries that we're going to use

---
+++ index.js
@@ -0,0 +1,9 @@
+// Export the requires modules that We'are going to used
+//Node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+//lib modules
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const generateTeam = require("./src/pageTEmplate");
